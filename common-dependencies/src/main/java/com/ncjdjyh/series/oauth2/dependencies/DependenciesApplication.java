0e27e66de2b85d23a5f02962ea05067c78b5dad6
jyh <ncjdjyh@outlook.com>
2019-08-09 16:01:02 +0800

    v1.1

---
+++ common-dependencies/src/main/java/com/ncjdjyh/series/oauth2/dependencies/DependenciesApplication.java
@@ -0,0 +1,16 @@
+package com.ncjdjyh.series.oauth2.dependencies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: ncjdjyh
+ * @FirstInitial: 2019/8/9
+ * @Description: ~
+ */
+@SpringBootApplication
+public class DependenciesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DependenciesApplication.class);
+    }
+}
